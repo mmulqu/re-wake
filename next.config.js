aa6782de120f6108eb06752482e7f40afad3d70a
Mmulqu <wakeanthunder@gmail.com>
2024-12-06 23:05:19 -0500

    config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Common Next.js configurations
+  reactStrictMode: true,
+  swcMinify: true,
+  
+  // If you're using experimental features
+  // experimental: {
+  //   serverActions: true,
+  // },
+  
+  // If you need to configure images from external sources
+  // images: {
+  //   domains: ['example.com'],
+  // },
+  
+  // If you need to add environment variables
+  env: {
+    // your env variables here
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
